5a7326a775c46b5673222debfbb6387c93c0f81d
Alex Blackman <alexander.blackman@gmail.com>
2021-01-18 14:07:27 +1100

    Add project files.

---
+++ overrideSafe/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace overrideSafe
+{
+    class JewelThief: Locksmith
+    {
+        private string stolenJewels;
+
+
+        new protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"I'm stealing the jewels! I stole: {stolenJewels}");
+        }
+    }
+}
---
+++ overrideSafe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace overrideSafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+
+            Locksmith calledAsLocksmith = new JewelThief();
+            calledAsLocksmith.ReturnContents(safeContents, owner);
+
+            JewelThief thief = new JewelThief();
+
+             thief.ReturnContents(safe, owner);
+
+            Console.ReadKey(true);
+        }
+    }
+}
